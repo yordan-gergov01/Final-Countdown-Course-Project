f234381d38311b320bd8a3847ab574e8743a4aa5
yordan-gergov01 <yordansoftskills@gmail.com>
2025-09-04 20:39:21 +0300

    connecting input with useRef

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,21 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const [userName, setUserName] = useState<string | null>(null);
+  const playerName = useRef<HTMLInputElement>(null);
+
+  function handleClick() {
+    if (playerName.current) {
+      setUserName(playerName.current?.value);
+    }
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {userName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
