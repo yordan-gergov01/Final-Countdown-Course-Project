14bd692124de1c3697f2e2453874f589b7adce6c
yordan-gergov01 <yordansoftskills@gmail.com>
2025-09-08 11:36:36 +0300

    showing the score

--- src/components/ResultModal.tsx
+++ src/components/ResultModal.tsx
@@ -10,6 +10,7 @@ function ResultModal(
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -21,6 +22,7 @@ function ResultModal(
   return (
     <dialog ref={dialogRef} className="result-modal">
       {userLost && <h2>You lost!</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
